998a5daae1f0c26d2957022be308cc92fb935fc8
taranvd <apple.vlad.taran@gmail.com>
2023-06-18 03:36:57 +0300

    done first, second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+const TIME_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(checkTimeUpdate, 1000));
+
+const savedTime = localStorage.getItem(TIME_KEY);
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
+
+function checkTimeUpdate({ seconds }) {
+  localStorage.setItem(TIME_KEY, seconds);
+}
