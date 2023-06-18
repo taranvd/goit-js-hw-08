1f7d928c9c3c76eafb76383fbd0a93412b64afa8
taranvd <apple.vlad.taran@gmail.com>
2023-06-18 15:30:27 +0300

    done third task :>

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+const throttle = require('lodash.throttle');
 
+const feedbackForm = document.querySelector('.feedback-form');
+const emailInputEl = document.querySelector('[name="email"]');
+const textareaEl = document.querySelector('[name="message"');
+
+feedbackForm.addEventListener('input', throttle(handleInputForm, 500));
+feedbackForm.addEventListener('submit', handleSubmitForm);
+
+const NAME_KEY = 'feedback-form-state';
+
+const formData = {};
+
+checkStatusLocalStorage();
+
+function handleInputForm(e) {
+  e.preventDefault();
+
+  formData[e.target.name] = e.target.value;
+
+  localStorage.setItem(NAME_KEY, JSON.stringify(formData));
+}
+
+function checkStatusLocalStorage() {
+  const savedResult = localStorage.getItem(NAME_KEY);
+  if (savedResult) {
+    const { email, message } = JSON.parse(savedResult);
+
+    textareaEl.value = message;
+    emailInputEl.value = email;
+  }
+}
+
+function handleSubmitForm() {
+  localStorage.removeItem(NAME_KEY);
+
+  console.log(formData);
+}
