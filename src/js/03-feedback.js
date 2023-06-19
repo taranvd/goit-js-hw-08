08cd70f2eeee66cbe94ea198e659abba622b419e
taranvd <apple.vlad.taran@gmail.com>
2023-06-19 12:33:32 +0300

    fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,21 +2,20 @@ const throttle = require('lodash.throttle');
 
 const feedbackForm = document.querySelector('.feedback-form');
 const emailInputEl = document.querySelector('[name="email"]');
-const textareaEl = document.querySelector('[name="message"');
+const textareaEl = document.querySelector('[name="message"]');
 
 feedbackForm.addEventListener('input', throttle(handleInputForm, 500));
 feedbackForm.addEventListener('submit', handleSubmitForm);
 
 const NAME_KEY = 'feedback-form-state';
 
-const formData = {};
-
 checkStatusLocalStorage();
 
-function handleInputForm(e) {
-  e.preventDefault();
-
-  formData[e.target.name] = e.target.value;
+function handleInputForm() {
+  const formData = {
+    email: emailInputEl.value,
+    message: textareaEl.value,
+  };
 
   localStorage.setItem(NAME_KEY, JSON.stringify(formData));
 }
@@ -31,8 +30,21 @@ function checkStatusLocalStorage() {
   }
 }
 
-function handleSubmitForm() {
+function handleSubmitForm(e) {
+  e.preventDefault();
+
+  if (!emailInputEl.value || !textareaEl.value) {
+    alert('Всі поля повинні бути заповнені');
+    return;
+  }
+
+  const formData = {
+    email: emailInputEl.value,
+    message: textareaEl.value,
+  };
+
   localStorage.removeItem(NAME_KEY);
 
+  feedbackForm.reset();
   console.log(formData);
 }
